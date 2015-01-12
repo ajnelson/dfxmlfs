151cae67bc225ea32ac59baeaf8312d744f6cfd4
Alex Nelson <a.nelson@prometheuscomputing.com>
2015-01-12 18:05:24 -0500

    Restart on simpler hello-world example

---
+++ hello.py
@@ -0,0 +1,1 @@
+deps/fuse-python/example/hello.py
--- test.sh
+++ test.sh
@@ -8,7 +8,7 @@ if [ ! -d testdir ]; then
 fi
 
 #Debug implies foreground mode, so sleep to allow mount to finish before using directory.
-python dfxmlfs.py -d -o xmlfile=test.xml testdir >stdout.log 2>stderr.log &
+python dfxmlfs-hello.py -d testdir >stdout.log 2>stderr.log & #-o xmlfile=test.xml 
 sleep 1
 
 ls testdir
