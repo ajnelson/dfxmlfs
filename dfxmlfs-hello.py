cc2f9893a1bce402a26ad3f8d6a8921050524228
Alex Nelson <a.nelson@prometheuscomputing.com>
2015-01-13 00:12:55 -0500

    Implement readdir()

--- dfxmlfs-hello.py
+++ dfxmlfs-hello.py
@@ -26,14 +26,17 @@ fuse.fuse_python_api = (0, 2)
 hello_path = '/hello'
 hello_str = 'Hello World!\n'
 
+_stat_fields = ['st_atime', 'st_ctime', 'st_dev', 'st_gid', 'st_ino', 'st_mode', 'st_mtime', 'st_nlink', 'st_size', 'st_uid']
 def obj_to_stat(obj):
     st = fuse.Stat()
+    #for field in _stat_fields:
+    #    _logger.debug("st.%s = %r." % (field, getattr(st, field)))
 
     st.st_ino = obj.inode
 
     st.st_dev = 0
 
-    st.st_nlink = obj.nlink
+    st.st_nlink = obj.nlink or 0 #In case of None
 
     st.st_size = obj.filesize
 
@@ -41,28 +44,33 @@ def obj_to_stat(obj):
     st.st_uid = 0
     st.st_gid = 0
 
-    if fo.name_type == "r":
+    if obj.name_type == "r":
         st.st_mode = 0o0444 | stat.S_IFREG
-    elif fo.name_type == "d":
+    elif obj.name_type == "d":
         st.st_mode = 0o0555 | stat.S_IFDIR
     else:
         st.st_mode = 0o0444
 
+    _logger.debug("Setting timestamps.")
     if obj.atime is None:
         st.st_atime = 0
     else:
-        st.st_atime = obj.atime.timestamp()
+        st.st_atime = obj.atime.timestamp
 
+    _logger.debug("Set a timestamp.")
     if obj.mtime is None:
         st.st_mtime = 0
     else:
-        st.st_mtime = obj.mtime.timestamp()
+        st.st_mtime = obj.mtime.timestamp
 
     if obj.crtime is None:
         st.st_ctime = 0
     else:
-        st.st_ctime = obj.crtime.timestamp()
+        st.st_ctime = obj.crtime.timestamp
 
+    _logger.debug("st = %r." % st)
+    #for field in _stat_fields:
+    #    _logger.debug("st.%s = %r." % (field, getattr(st, field)))
     return st
 
 class HelloFS(fuse.Fuse):
@@ -72,14 +80,18 @@ class HelloFS(fuse.Fuse):
             raise RuntimeError("-o xmlfile must be passed on the command line.")
 
         _logger.debug("Parsing DFXML file...")
+
+        #Key: Absolute path
+        #Value: Objects.FileObject
         self.objects_by_path = dict()
+
         for (event, obj) in Objects.iterparse(self.xmlfile):
             if not isinstance(obj, Objects.FileObject):
                 continue
             _logger.debug("File: %r." % obj.filename)
-            self.objects_by_path[obj.filename] = obj
+            self.objects_by_path["/" + obj.filename] = obj
         _logger.debug("Parsed DFXML file.")
-        _logger.debug("self.objects_by_path = %r." % self.objects_by_path)
+        #_logger.debug("self.objects_by_path = %r." % self.objects_by_path)
 
         return fuse.Fuse.main(self)
 
@@ -93,11 +105,14 @@ class HelloFS(fuse.Fuse):
             if obj is None:
                 return -errno.ENOENT
             st = obj_to_stat(obj)
+        _logger.debug("dir(st) = %r." % dir(st))
         return st
 
     def readdir(self, path, offset):
-        for r in  '.', '..', hello_path[1:]:
+        for r in  '.', '..':
             yield fuse.Direntry(r)
+        for filename in self.objects_by_path:
+            yield fuse.Direntry(filename[1:])
 
     def open(self, path, flags):
         #Existence check
