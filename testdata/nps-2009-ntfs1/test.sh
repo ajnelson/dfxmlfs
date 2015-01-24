122e2ea65905658d1be4522a69c8effae275a70a
Alex Nelson <a.nelson@prometheuscomputing.com>
2015-01-24 16:02:48 -0500

    Add read test

---
+++ testdata/nps-2009-ntfs1/test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+set -x
+
+if [ ! -d testdir ]; then
+  mkdir testdir
+fi
+
+#Debug implies foreground mode, so sleep to allow mount to finish before using directory.
+python2.7 dfxmlfs.py \
+  -d \
+  -o xmlfile=ntfs1-gen1.E01.dfxml \
+  -o imgfile=ntfs1-gen1.E01 \
+  testdir \
+  >stdout.log 2>stderr.log &
+sleep 1
+
+#Disable halt-on-errors here to allow cleanup script to run.
+set +e
+
+python test_reads.py --debug ntfs1-gen1.E01.dfxml testdir
+
+./cleanup.sh
+sleep 1
---
+++ testdata/nps-2009-ntfs1/test_reads.py
@@ -0,0 +1,61 @@
+
+#Run in Python 2 or Python 3.
+
+import logging
+import os
+import hashlib
+import sys
+
+_logger = logging.getLogger(os.path.basename(__file__))
+
+import Objects
+
+def main():
+    hash_mismatches = 0
+    for (event, obj) in Objects.iterparse(args.xmlfile):
+        if not isinstance(obj, Objects.FileObject):
+            continue
+        if not obj.filename:
+            continue
+        if not obj.filename.startswith("RAW/"):
+            continue
+        if obj.name_type != "r":
+            continue
+
+        if obj.sha1 is None:
+            continue
+
+        path = os.path.join(args.testdir, "partition_1", obj.filename)
+        _logger.debug("Inspecting path: %r." % path)
+        _logger.debug("Recreating SHA-1: %r." % obj.sha1)
+
+        checker = hashlib.sha1()
+        with open(path, "rb") as fh:
+            while True:
+                buf = fh.read(4096)
+                if len(buf) == 0:
+                    break
+                checker.update(buf)
+        checker_sha1 = checker.hexdigest().lower()
+        _logger.debug("Hash of content read from file system: %r." % checker_sha1)
+
+        if obj.sha1 != checker_sha1:
+            hash_mismatches += 1
+            _logger.error("Hash mismatch on path: %r." % path)
+            _logger.debug("obj.id = %r." % obj.id)
+
+    if hash_mismatches != 0:
+        _logger.error("Read incorrect content on %d files." % hash_mismatches)
+        sys.exit(1)
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--debug", action="store_true")
+    parser.add_argument("xmlfile")
+    parser.add_argument("testdir")
+    args = parser.parse_args()
+
+    logging.basicConfig(level=logging.DEBUG if args.debug else logging.INFO)
+
+    main()
